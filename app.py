f1d73e7677f0eee3ffc5072487a2d435cea45f65
nicoValencia25 <nvalencia@fi.uba.ar>
2024-11-03 11:52:37 -0300

    base.html
    creacion del archivo base.html, extends en todos los templates y min cambios en app.py para q funcione

--- app.py
+++ app.py
@@ -10,47 +10,47 @@ def home():
 @app.route('/vacaciones')
 def provincias():
     
-    return render_template('provincias.html')
+    return render_template('destination.html')
 
 @app.route('/vacaciones/<string:provincia>')
 def hoteles(provincia):
 
-    return render_template('hoteles.html', provincia=provincia)
+    return render_template('booking.html', provincia=provincia)
 
-@app.route('vacaciones/<string:provincia>/<string:hotel>')
+@app.route('/vacaciones/<string:provincia>/<string:hotel>')
 def habitaciones(hotel):
     
     return render_template('habitaciones.html', hotel=hotel)
 
-@app.route('vacaciones/<string:provincia>/<string:hotel>/<string:habitacion>')
+@app.route('/vacaciones/<string:provincia>/<string:hotel>/<string:habitacion>')
 def reservar(habitacion):
     
     return render_template('reservar.html', habitacion=habitacion)
 
-@app.route('reservas')
+@app.route('/reservas')
 def reservas():
     
-    return render_template('reservas.html')
+    return render_template('reservas_act.html')
 
-@app.route('reservas/<string:reserva>/editar_reserva')
+@app.route('/reservas/<string:reserva>/editar_reserva')
 def editar_reserva(reserva):
     
     return render_template('editar_reserva.html', reserva=reserva)
 
-@app.route('reservas/<string:reserva>/cancelar_reserva')
+@app.route('/reservas/<string:reserva>/cancelar_reserva')
 def cancelar_reserva(reserva):
     
     return render_template('cancelar_reserva.html', reserva=reserva)
 
-@app.route('sobre_nosotros')
+@app.route('/sobre_nosotros')
 def sobre_nosotros():
     
-    return render_template('sobre_nosotros.html')
+    return render_template('about.html')
 
-@app.route('contacto')
+@app.route('/contacto')
 def contacto():
     
-    return render_template('contacto.html')
+    return render_template('contact.html')
 
 if __name__=='__main__':
     app.run(debug=True)
