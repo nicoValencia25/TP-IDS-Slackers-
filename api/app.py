e585bedab60932ed0759affb47cd5912fbb50aaf
Iván, Nicolás Ignacio <nivan@fi.uba.ar>
2024-11-03 22:12:24 -0300

    #25 Reserva por ID

--- api/app.py
+++ api/app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+import reservas
 
 app = Flask(__name__)
 
+
+@app.route("/api/v1/reservas/<int:res_id>", methods=["GET"])
+def get_reservas_by_id(res_id):
+    try:
+        result = reservas.reservas_by_id(res_id)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    if len(result) == 0:
+        return jsonify({"error": "No se encontró ninguna reserva"}), 404
+
+    result = result[0]
+    return (
+        jsonify(
+            {
+                "UsuarioID": result[0],
+                "HabitacionID": result[1],
+                "ReservaCreacion": result[2],
+                "Desde": result[3],
+                "Hasta": result[4],
+                "CantAdultos": result[5],
+                "CantNiños": result[6],
+                "PrecioTotal": result[7],
+            }
+        ),
+        200,
+    )
 
 
 if __name__ == "__main__":
---
+++ api/reservas.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+QUERY_RESERVAS_BY_ID = """
+SELECT UsuarioID, HabitacionID, ReservaCreacion, Desde, Hasta, CantAdultos, CantNiños, PrecioTotal
+FROM Reservas
+WHERE ReservaID = :res_id
+"""
+
+engine = create_engine("mysql://root:root@localhost:3306/HotelDB")
+# engine = create_engine("mysql+mysqlconnector://root@localhost:3306/HotelDB")
+
+
+def run_query(query, parameters=None):
+    with engine.connect() as conn:
+        result = conn.execute(text(query), parameters)
+        conn.commit()
+
+    return result
+
+
+def reservas_by_id(res_id):
+    return run_query(QUERY_RESERVAS_BY_ID, {"res_id": res_id}).fetchall()
