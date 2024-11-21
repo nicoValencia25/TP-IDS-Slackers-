f08d447c7a3ccf898e41bfe7a5c107826e892cb4
Iván, Nicolás Ignacio <nivan@fi.uba.ar>
2024-11-21 18:34:23 -0300

    #36 Vista hoteles

---
+++ app_android/app/src/main/java/com/tp_ids/hoteles/hotel/HotelViewHolder.kt
@@ -0,0 +1,21 @@
+package com.tp_ids.hoteles.hotel
+
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.tp_ids.hoteles.R
+
+class HotelViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val tvHotelName: TextView = view.findViewById(R.id.tvHotelName)
+    private val ivHotel: ImageView = view.findViewById(R.id.ivHotel)
+    private val tvHotelDescription: TextView = view.findViewById(R.id.tvHotelDescription)
+    private val btnHotel: Button = view.findViewById(R.id.btnHotel)
+
+    fun render(hotel: Hotel) {
+        tvHotelName.text = hotel.name
+        // TODO poner la imagen
+        tvHotelDescription.text = hotel.description
+    }
+}
