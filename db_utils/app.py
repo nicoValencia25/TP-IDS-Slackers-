b9e97cde0c8d5cc02785dbdb0627c6c48d62f436
jbarbieri-fiuba <jbarbieri@fi.uba.ar>
2024-11-21 00:50:43 -0300

    Added more queries to reservas.py
    app.py will be provisionally ignored until we find out a way to run it without having to get another website.
    reservas.py will provisionally contain the "Habitaciones" queries until I find a better way to share a db interface between both without passing it as a parameter in function calls.

---
+++ db_utils/app.py
@@ -0,0 +1,136 @@
+from flask import Flask, jsonify, request
+from sqlalchemy.exc import SQLAlchemyError
+
+from reservas import db, reservas_all, reservas_by_id, reservas_add, reservas_remove
+
+app = Flask(__name__)
+
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<DB_USER>:<DB_PASSWORD>@<DB_HOST>/<DB_NAME>'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning from SQLAlchemy
+
+db.init_app(app)
+
+@app.route("/api/v1/reservas", methods=["GET"])
+def get_reservas():
+    try:
+        result = reservas_all()
+    except SQLAlchemyError as e:
+        return jsonify({"error": str(e)}), 500
+
+    response = []
+    for row in result:
+        response.append(
+            jsonify(
+                {
+                    "ReservaID": row[0],
+                    "Creacion": row[1],
+                    "Desde": row[2],
+                    "Hasta": row[3],
+                    "CantNiños": row[4],
+                    "CantAdultos": row[5],
+                    "PrecioTotal": row[6],
+                    "HabID": row[7],
+                    "UsuarioID": row[8],
+                }
+            )
+        )
+
+    return jsonify(response), 200
+
+
+@app.route("/api/v1/reservas/<int:res_id>", methods=["GET"])
+def get_reservas_by_id(res_id):
+    try:
+        result = reservas_by_id(res_id)
+    except SQLAlchemyError as e:
+        return jsonify({"error": str(e)}), 500
+
+    if len(result) == 0:
+        return jsonify({"error": "No se encontró ninguna reserva"}), 404
+
+    result = result[0]
+    return (
+        jsonify(
+            {
+                "ReservaID": result[0],
+                "Creacion": result[1],
+                "Desde": result[2],
+                "Hasta": result[3],
+                "CantNiños": result[4],
+                "CantAdultos": result[5],
+                "PrecioTotal": result[6],
+                "HabID": result[7],
+                "UsuarioID": result[8],
+            }
+        ),
+        200,
+    )
+
+
+@app.route("/api/v1/reservas", methods=["POST"])
+def post_reserva():
+    data = request.get_json()
+
+    keys = (
+        "ReservaID",
+        "Creacion",
+        "Desde",
+        "Hasta",
+        "CantNiños",
+        "CantAdultos",
+        "PrecioTotal",
+        "HabID",
+        "UsuarioID",
+    )
+
+    for key in keys:
+        if key not in data:
+            return jsonify({"error": f"Faltan el dato {key}"}), 400
+
+    try:
+        result = reservas_by_id(data["ReservaID"])
+        if len(result) > 0:
+            return jsonify({"error": "Existe una reserva con ese mismo ID"}), 400
+
+        reservas_add(data)
+
+    except SQLAlchemyError as e:
+        return jsonify({"error": str(e)}), 500
+
+    return jsonify(data), 201
+
+
+@app.route('/api/v1/reservas/<int:res_id>', methods=['DELETE'])
+def delete_reservas(res_id):
+    try:
+        result = reservas_by_id(res_id)
+        if len(result) == 0:
+            return jsonify({'error': 'No se encontró la reserva'}), 404
+
+        reservas_remove(res_id)
+
+    except SQLAlchemyError as e:
+        return jsonify({'error': str(e)}), 500
+
+    result = result[0]
+    return (
+        jsonify(
+            {
+                "ReservaID": res_id,
+                "Creacion": result[1],
+                "Desde": result[2],
+                "Hasta": result[3],
+                "CantNiños": result[4],
+                "CantAdultos": result[5],
+                "PrecioTotal": result[6],
+                "HabID": result[7],
+                "UsuarioID": result[8],
+            }
+        ),
+        200,
+    )
+
+if __name__ == "__main__":
+    app.run(port="5001", debug=True)
