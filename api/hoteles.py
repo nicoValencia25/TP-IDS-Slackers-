36a5669556cc273d006293f957b6f0621b32a870
Iván, Nicolás Ignacio <nivan@fi.uba.ar>
2024-12-02 22:56:28 -0300

    QUERY_HOTELID_BY_HABID solucionado problemas de ambiguedad

--- api/hoteles.py
+++ api/hoteles.py
@@ -25,7 +25,7 @@ DELETE FROM Hoteles WHERE HotelID = :HotelID
 """
 
 QUERY_HOTELID_BY_HABID = """
-SELECT Hoteles.HotelID, Nombre, Provincia, Descripcion, Direccion, CodigoPostal, Localidad, Longitud, Latitud
+SELECT Hoteles.HotelID, Hoteles.Nombre, Provincia, Hoteles.Descripcion, Direccion, CodigoPostal, Localidad, Longitud, Latitud
 FROM Hoteles
 INNER JOIN TiposDeHabitacion on TiposDeHabitacion.HotelID = Hoteles.HotelID
 INNER JOIN Habitaciones on Habitaciones.TipoID = TiposDeHabitacion.TipoID
